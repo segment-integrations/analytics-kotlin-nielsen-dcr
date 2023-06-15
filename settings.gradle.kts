3702144bf02b12bcbd5d29665e5f93a73b85db81
Michael Grosse Huelsewiesche <mihuelsewiesche@twilio.com>
2023-06-15 18:20:49 -0400

    Preparing to rename repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "analytics-kotlin-integration-nielsen-dcr"
+rootProject.name = "analytics-kotlin-nielsen-dcr"
 include(":lib")
 includeBuild("publishing-plugins")
 include(":testapp")
